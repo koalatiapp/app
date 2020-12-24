485a5766165528d999fe9b53b410796f7300412f
Émile Perron <contact@emileperron.com>
2020-12-23 20:40:52 -0500

    Added fix for PHP-CS-Fixer CI script

--- DEV/ci/php-cs-fixer.sh
+++ DEV/ci/php-cs-fixer.sh
@@ -1,6 +1,13 @@
+if git rev-parse --verify HEAD >/dev/null 2>&1
+then
+	against=HEAD~..HEAD
+else
+	against=$(git hash-object -t tree /dev/null)
+fi
+
 IFS='
 '
-CHANGED_FILES=$(git diff --name-only --diff-filter=ACMRTUXB HEAD~..HEAD)
+CHANGED_FILES=$(git diff --name-only --diff-filter=ACMRTUXB ${against})
 if ! echo "${CHANGED_FILES}" | grep -qE "^(\\.php_cs(\\.dist)?|composer\\.lock)$"; then EXTRA_ARGS=$(printf -- '--path-mode=intersection\n--\n%s' "${CHANGED_FILES}"); else EXTRA_ARGS=''; fi
 # TODO: Remove the "PHP_CS_FIXER_IGNORE_ENV=1" when PHP-CS-Fixer starts supporting PHP 8
 PHP_CS_FIXER_IGNORE_ENV=1 vendor/bin/php-cs-fixer fix --config=.php_cs.dist -v --dry-run --stop-on-violation --using-cache=no ${EXTRA_ARGS}
