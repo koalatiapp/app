83337593f59cc16a7411faeaac537bd1eff48442
Émile Perron <contact@emileperron.com>
2020-12-23 20:37:29 -0500

    Added temporary fix for PHP-CS-Fixer missing PHP 8 support in CI script

--- DEV/ci/php-cs-fixer.sh
+++ DEV/ci/php-cs-fixer.sh
@@ -2,4 +2,5 @@ IFS='
 '
 CHANGED_FILES=$(git diff --name-only --diff-filter=ACMRTUXB HEAD~..HEAD)
 if ! echo "${CHANGED_FILES}" | grep -qE "^(\\.php_cs(\\.dist)?|composer\\.lock)$"; then EXTRA_ARGS=$(printf -- '--path-mode=intersection\n--\n%s' "${CHANGED_FILES}"); else EXTRA_ARGS=''; fi
-vendor/bin/php-cs-fixer fix --config=.php_cs.dist -v --dry-run --stop-on-violation --using-cache=no ${EXTRA_ARGS}
+# TODO: Remove the "PHP_CS_FIXER_IGNORE_ENV=1" when PHP-CS-Fixer starts supporting PHP 8
+PHP_CS_FIXER_IGNORE_ENV=1 vendor/bin/php-cs-fixer fix --config=.php_cs.dist -v --dry-run --stop-on-violation --using-cache=no ${EXTRA_ARGS}
