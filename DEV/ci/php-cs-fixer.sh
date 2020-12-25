0f293bf3eb922c9c7b166c782e931c9e280f58d9
Émile Perron <contact@emileperron.com>
2020-12-24 23:26:34 -0500

    Updated CI scripts and pre-commit, and their associated configurations, to fix issue and better match the desired standards

--- DEV/ci/php-cs-fixer.sh
+++ DEV/ci/php-cs-fixer.sh
@@ -10,4 +10,10 @@ IFS='
 CHANGED_FILES=$(git diff --name-only --diff-filter=ACMRTUXB ${against})
 if ! echo "${CHANGED_FILES}" | grep -qE "^(\\.php_cs(\\.dist)?|composer\\.lock)$"; then EXTRA_ARGS=$(printf -- '--path-mode=intersection\n--\n%s' "${CHANGED_FILES}"); else EXTRA_ARGS=''; fi
 # TODO: Remove the "PHP_CS_FIXER_IGNORE_ENV=1" when PHP-CS-Fixer starts supporting PHP 8
-PHP_CS_FIXER_IGNORE_ENV=1 vendor/bin/php-cs-fixer fix --config=.php_cs.dist -v --dry-run --stop-on-violation --using-cache=no ${EXTRA_ARGS}
+PHP_CS_FIXER_IGNORE_ENV=1 vendor/bin/php-cs-fixer fix --config=.php_cs.dist -vvv --dry-run --stop-on-violation --using-cache=no ${EXTRA_ARGS}
+
+if [ $? -ne 0 ]; then
+		echo "\nRun the following command to attempt automatic fix:"
+		echo "vendor/bin/php-cs-fixer fix --config=.php_cs.dist -vvv"
+		exit 1
+fi
--- DEV/ci/phpstan.sh
+++ DEV/ci/phpstan.sh
@@ -1,1 +1,1 @@
-vendor/bin/phpstan analyse -n
+php -d memory_limit=-1 vendor/bin/phpstan analyse -n
