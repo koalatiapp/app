8d8aa2cc719e9838198c355246e9add380139feb
Émile Perron <contact@emileperron.com>
2020-12-27 21:34:12 -0500

    Edited CI workflow to execute unit & functional tests after the docker build

--- config/bundles.php
+++ config/bundles.php
@@ -11,4 +11,5 @@ return [
 	Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 	FOS\JsRoutingBundle\FOSJsRoutingBundle::class => ['all' => true],
 	Pyrrah\GravatarBundle\PyrrahGravatarBundle::class => ['all' => true],
+	DAMA\DoctrineTestBundle\DAMADoctrineTestBundle::class => ['test' => true],
 ];
