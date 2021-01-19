78afe14c1de3ea455b024af041b48a5ecad489b0
Émile Perron <contact@emileperron.com>
2021-01-18 21:28:44 -0500

    Updated some CI scripts to only run on added files when in a git pre-commit context.

--- DEV/ci/phpmd.sh
+++ DEV/ci/phpmd.sh
@@ -1,1 +1,8 @@
-vendor/phpmd/phpmd/src/bin/phpmd src text .phpmd.xml
+addedFiles=`echo "$(git diff --diff-filter=d --cached --name-only)" | paste -s -d, /dev/stdin`
+
+if [ -z "$addedFiles" ];
+then
+	vendor/phpmd/phpmd/src/bin/phpmd $addedFiles text .phpmd.xml
+else
+	vendor/phpmd/phpmd/src/bin/phpmd src text .phpmd.xml
+fi
--- DEV/ci/phpstan.sh
+++ DEV/ci/phpstan.sh
@@ -1,1 +1,8 @@
-php -d memory_limit=-1 vendor/bin/phpstan analyse -n
+addedFiles=$(git diff --diff-filter=d --cached --name-only)
+
+if [ -z "$addedFiles" ];
+then
+	php -d memory_limit=-1 vendor/bin/phpstan analyse -n
+else
+	php -d memory_limit=-1 vendor/bin/phpstan analyse -n $addedFiles
+fi
