735dc7c0282cf22565f1f5a94bbfa75fe868b586
Émile Perron <contact@emileperron.com>
2021-01-19 08:05:13 -0500

    Fixed inversed empty-check in if for phpmd CI script.

--- DEV/ci/phpmd.sh
+++ DEV/ci/phpmd.sh
@@ -2,7 +2,7 @@ addedFiles=`echo "$(git diff --diff-filter=d --cached --name-only)" | paste -s -
 
 if [ -z "$addedFiles" ];
 then
-	vendor/phpmd/phpmd/src/bin/phpmd $addedFiles text .phpmd.xml
-else
 	vendor/phpmd/phpmd/src/bin/phpmd src text .phpmd.xml
+else
+	vendor/phpmd/phpmd/src/bin/phpmd $addedFiles text .phpmd.xml
 fi
