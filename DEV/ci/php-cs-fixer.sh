f9e2a41381748f11b695947ebfe4400fe1d4c5d9
Émile Perron <contact@emileperron.com>
2021-03-17 19:14:17 -0400

    Updated php-cs-fixer script to prevent failure when deleting files

--- DEV/ci/php-cs-fixer.sh
+++ DEV/ci/php-cs-fixer.sh
@@ -1,13 +1,13 @@
 if [ -z "$GITHUB_SHA" ]
 then
-	against=HEAD~..HEAD
+	against=""
 else
 	against="$GITHUB_SHA"
 fi
 
 IFS='
 '
-CHANGED_FILES=$(git diff --name-only --diff-filter=ACMRTUXB ${against})
+CHANGED_FILES=$(git diff --name-only --diff-filter=ACMRTUXBd ${against})
 if ! echo "${CHANGED_FILES}" | grep -qE "^(\\.php_cs(\\.dist)?|composer\\.lock)$"; then EXTRA_ARGS=$(printf -- '--path-mode=intersection\n--\n%s' "${CHANGED_FILES}"); else EXTRA_ARGS=''; fi
 # TODO: Remove the "PHP_CS_FIXER_IGNORE_ENV=1" when PHP-CS-Fixer starts supporting PHP 8
 PHP_CS_FIXER_IGNORE_ENV=1 vendor/bin/php-cs-fixer fix --config=.php_cs.dist -vvv --dry-run --stop-on-violation --using-cache=no ${EXTRA_ARGS}
