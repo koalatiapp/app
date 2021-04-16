80db86e726f9efa7780262214cbaddb01b290e5c
Émile Perron <contact@emileperron.com>
2021-04-15 21:46:31 -0400

    Worked on modals some more...

--- public/js/base/modal.js
+++ public/js/base/modal.js
@@ -1,6 +1,7 @@
 /**
  * Keeps a reference to every active modal.
  * This is used to close all modals at once.
+ * @type {Modal[]}
  */
 const modalStack = [];
 
@@ -24,13 +25,16 @@ export default class Modal {
 
 		this.originalFocus = document.activeElement;
 		this.guid = this._generateGuid();
-		this.options = this.__standardizeOptions(options);
+		this.options = this._standardizeOptions(options);
 		this.wrapperElement = this._createWrapper();
 		this.dialogElement = this._createDialog();
+		this.contentElement = this.dialogElement.querySelector(".modal-content");
 		this._initEventListeners();
 		this._loadContent();
 
 		modalStack.push(this);
+
+		setTimeout(() => { this.show(); }, 10);
 	}
 
 	/**
@@ -43,7 +47,7 @@ export default class Modal {
 	 */
 	_standardizeOptions(options)
 	{
-		if (typeof options.content != "string") {
+		if (["undefined", "string"].indexOf(typeof options.content) == -1) {
 			options.content = Promise.resolve(options.content);
 		}
 
@@ -71,7 +75,9 @@ export default class Modal {
 
 		const backdrop = document.createElement("div");
 		backdrop.className = "modal-backdrop";
+
 		wrapper.appendChild(backdrop);
+		document.body.appendChild(wrapper);
 
 		return wrapper;
 	}
@@ -83,21 +89,23 @@ export default class Modal {
 	_createDialog()
 	{
 		const dialog = document.createElement("div");
+		dialog.className = "modal";
 		dialog.setAttribute("role", "dialog");
 		dialog.setAttribute("aria-labelledby", `modal-title-${this.guid}`);
 		dialog.setAttribute("aria-describedby", `modal-content-${this.guid}`);
+		dialog.setAttribute("aria-hidden", "true");
 		dialog.innerHTML = `
-			<header>
-				<div class="modal-title" id="modal-title-${this.guid}">${this.options?.title}</div>
+			<header class="modal-header">
+				<div class="modal-title" id="modal-title-${this.guid}">${this.options.title ?? ""}</div>
 				<div class="modal-actions">
-					<button type="button" class="modal-close" aria-label="Close this dialog">
+					<button type="button" class="modal-close small icon-only" aria-label="Close this dialog">
 						<i class="far fa-times" aria-hidden="true"></i>
 					</button>
 				</div>
 			</header>
-			<div class="modal-content" id="modal-content-${this.guid}">${this.options.content instanceof Promise ? "" : this.options?.content}</div>
+			<div class="modal-content" id="modal-content-${this.guid}">${this.options.content instanceof Promise ? "" : (this.options?.content ?? "")}</div>
 		`;
-		this.wrapperElement.appendChild(dialog);
+		this.wrapperElement.prepend(dialog);
 
 		return dialog;
 	}
@@ -111,15 +119,26 @@ export default class Modal {
 			return;
 		}
 
+		this.toggleLoading(true);
+
 		const contentElement = this.dialogElement.querySelector(".modal-content");
+		let contentPromise;
 
 		if (this.options?.content) {
-			this.options.content.then(content => contentElement.innerHTML = content);
+			// Use the content promise
+			contentPromise = this.options.content;
 		} else {
-			fetch(this.options?.contentUrl)
-				.then(response => response.text())
-				.then(content => contentElement.innerHTML = content);
+			// Fetch the content from the provided URL
+			contentPromise = fetch(this.options?.contentUrl).then(response => response.text());
 		}
+
+		contentPromise.then(content => {
+			contentElement.innerHTML = content;
+		}).catch(() => {
+			contentElement.innerHTML = "Sorry, an error occured while loading this content. Please try again.";
+		}).finally(() => {
+			this.toggleLoading(false);
+		});
 	}
 
 	/**
@@ -127,7 +146,7 @@ export default class Modal {
 	 */
 	_initEventListeners()
 	{
-		this.dialogElement.querySelector("header > button.modal-close").addEventListener("click", (e) => {
+		this.dialogElement.querySelector("button.modal-close").addEventListener("click", (e) => {
 			e.preventDefault();
 			this.close();
 		});
@@ -140,6 +159,8 @@ export default class Modal {
 	show()
 	{
 		// @TODO: Open the modal
+		this.dialogElement.setAttribute("aria-hidden", "false");
+
 		// @TODO: Focus the first focusable element in the modal
 	}
 
@@ -167,6 +188,24 @@ export default class Modal {
 	}
 
 	/**
+	 * Toggles the loading state for the modal's content.
+	 *
+	 * @param {boolean} [forcedState] - Forces a specific state (`true“ enables the loading, `false` disables it)
+	 */
+	toggleLoading(forcedState)
+	{
+		const isLoading = forcedState ?? this.contentElement.getAttribute("aria-busy") != "true";
+
+		if (isLoading) {
+			this.contentElement.setAttribute("aria-live", "polite");
+		} else {
+			this.contentElement.removeAttribute("aria-live");
+		}
+
+		this.contentElement.setAttribute("aria-busy", isLoading ? "true" : "false");
+	}
+
+	/**
 	 * Closes every active modal.
 	 *
 	 * @param {boolean} [skipConfirm=false] - Whether confirmations should be skipped when closing modals that require confirmation on-close.
@@ -176,5 +215,15 @@ export default class Modal {
 	{
 		// @TODO: Loop over every modal in the `modalStack`, calling and awaiting `close(skipConfirm)` for each of them
 		// @TODO: Return a promise with a boolean indicating if the modal is closed or not
+	}
+
+	/**
+	 * Returns the currently active modal instance.
+	 *
+	 * @returns {Modal|null} Instance of the currently active modal (or null if none is active)
+	 */
+	static getCurrent()
+	{
+		return modalStack[modalStack.length - 1] ?? null;
 	}
 }
