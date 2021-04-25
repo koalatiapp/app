0f3e9a56d464bb7b93a1819dc4bbbd1d0a08fc20
Émile Perron <contact@emileperron.com>
2021-04-25 15:25:13 -0400

    Updated project settings to only re-send messages if the URL has changed

--- src/Controller/Project/ProjectSettingsController.php
+++ src/Controller/Project/ProjectSettingsController.php
@@ -2,10 +2,13 @@
 
 namespace App\Controller\Project;
 
+use App\Entity\Project;
 use App\Form\Project\ProjectSettingsType;
 use App\Message\FaviconRequest;
 use App\Message\ScreenshotRequest;
+use App\Message\SitemapRequest;
 use App\Util\Url;
+use Symfony\Component\Form\Form;
 use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -30,7 +33,7 @@ class ProjectSettingsController extends AbstractProjectController
 
 		if ($form->isSubmitted()) {
 			// Handle deletion first without regular form validation
-			if ($form->getClickedButton()->getName() == 'delete' && $this->isCsrfTokenValid($form->getName(), $request->request->get($form->getName())['_token'] ?? null)) {
+			if ($this->isRequestingDeletion($form, $request)) {
 				if ($form->get('deleteConfirmation')->getData() === true) {
 					$em = $this->getDoctrine()->getManager();
 					$em->remove($project);
@@ -39,31 +42,13 @@ class ProjectSettingsController extends AbstractProjectController
 					$this->addFlash('success', $translator->trans('project_settings.flash.deleted_successfully', ['%name%' => $project->getName()]));
 
 					return $this->redirectToRoute('dashboard');
-				} else {
-					$form->get('deleteConfirmation')->addError(new FormError($translator->trans('project_settings.delete.error_confirmation')));
 				}
+
+				$form->get('deleteConfirmation')->addError(new FormError($translator->trans('project_settings.delete.error_confirmation')));
 			}
 			// Handle the good old form settings form regularly
 			elseif ($form->isValid()) {
-				$websiteUrl = $urlHelper->standardize($project->getUrl());
-
-				if ($originalProject->getUrl() != $project->getUrl()) {
-					// Check if the provided website URL exists
-					if (!$urlHelper->exists($websiteUrl)) {
-						$form->get('url')->addError(new FormError($translator->trans('project_settings.form.field.url.error_unreachable')));
-					}
-				}
-
-				if ($form->isValid()) {
-					$project->setUrl($websiteUrl);
-					$em = $this->getDoctrine()->getManager();
-					$em->persist($project);
-					$em->flush();
-
-					$this->addFlash('success', $translator->trans('project_settings.flash.updated_successfully', ['%name%' => $project->getName()]));
-					$this->dispatchMessage(new ScreenshotRequest($project->getId()));
-					$this->dispatchMessage(new FaviconRequest($project->getId()));
-				}
+				$this->processChanges($form, $project, $originalProject, $urlHelper, $translator);
 			}
 		}
 
@@ -71,5 +56,39 @@ class ProjectSettingsController extends AbstractProjectController
 			'project' => $project,
 			'form' => $form->createView(),
 		]);
+	}
+
+	private function processChanges(Form $form, Project $project, Project $originalProject, Url $urlHelper, TranslatorInterface $translator): void
+	{
+		$websiteUrl = $urlHelper->standardize($project->getUrl());
+		$urlHasChanged = $originalProject->getUrl() != $project->getUrl();
+
+		// Check if the provided website URL exists
+		if ($urlHasChanged && !$urlHelper->exists($websiteUrl)) {
+			$form->get('url')->addError(new FormError($translator->trans('project_settings.form.field.url.error_unreachable')));
+		}
+
+		if ($form->isValid()) {
+			$project->setUrl($websiteUrl);
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($project);
+			$em->flush();
+
+			$this->addFlash('success', $translator->trans('project_settings.flash.updated_successfully', ['%name%' => $project->getName()]));
+
+			if ($urlHasChanged) {
+				$this->dispatchMessage(new ScreenshotRequest($project->getId()));
+				$this->dispatchMessage(new FaviconRequest($project->getId()));
+				$this->dispatchMessage(new SitemapRequest($project->getId()));
+			}
+		}
+	}
+
+	private function isRequestingDeletion(Form $form, Request $request): bool
+	{
+		$clickedButtonName = $form->getClickedButton()->getName();
+		$csrfToken = $request->request->get($form->getName())['_token'] ?? null;
+
+		return $clickedButtonName == 'delete' && $this->isCsrfTokenValid($form->getName(), $csrfToken);
 	}
 }
