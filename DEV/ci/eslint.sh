ae0fc30ad9321b3d4cf9b8b726d0aff48e8d6fac
Émile Perron <contact@emileperron.com>
2021-04-28 21:35:04 -0400

    Updated eslint CI to run on assets directory

--- DEV/ci/eslint.sh
+++ DEV/ci/eslint.sh
@@ -1,7 +1,13 @@
 # Only run eslint if the public/js/ directory contains files
 if [ -n "$(ls -A public/js/)" ]; then
-	echo "Running ESLint on Javascript files..."
+	echo "Running ESLint on Javascript files from public/js/..."
 	node_modules/eslint/bin/eslint.js public/js/
+fi
+
+# Only run eslint if the assets directory contains files
+if [ -n "$(ls -A assets/)" ]; then
+	echo "Running ESLint on Javascript files from assets/..."
+	node_modules/eslint/bin/eslint.js assets/
 fi
 
 # Only run eslint if the templates directory contains files
