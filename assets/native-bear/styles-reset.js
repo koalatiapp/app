be51801c70ef6d8c9776cdb3fd47f802178178c7
Émile Perron <contact@emileperron.com>
2021-05-23 18:37:16 -0400

    Adds base styles to the reset/base for web components

--- assets/native-bear/styles-reset.js
+++ assets/native-bear/styles-reset.js
@@ -4,4 +4,18 @@ export default css`
 	:host { box-sizing: border-box; -webkit-font-smoothing: inherit; }
 	* { font-family: inherit; font-size: inherit; box-sizing: inherit; -webkit-font-smoothing: inherit; }
 	a { text-decoration: underline; }
+	p { line-height: 1.4; }
+	h1,
+	h2,
+	h3,
+	h4,
+	h5,
+	h6 { margin: 0 auto; }
+	h1 { font-size: 1.75rem; font-weight: 700; line-height: 1.5; }
+	h2 { margin-bottom: 1.5em; font-size: 1.2rem; font-weight: 600; line-height: 1.25; }
+	h3 { font-size: 1.2rem; font-weight: 500; line-height: 1.25; }
+	strong { font-weight: 600; }
+	hr,
+	.spacer { width: 100%; height: 1px; margin: 30px auto; background-color: #F4F4F4; border: none; }
+	.spacer { height: 0; }
 `;
