0f91efafb9dca4b1b34081fce1aa8e3dccfec259
Émile Perron <contact@emileperron.com>
2021-06-02 09:11:53 -0400

    Hides default summary toggle icon for Safari

--- assets/native-bear/accordion/accordion.js
+++ assets/native-bear/accordion/accordion.js
@@ -7,6 +7,7 @@ export class NbAccordion extends LitElement {
 			:host { display: block; margin-top: 1rem; }
 			details { font-size: .85rem; background-color: var(--color-gray-lighter); box-shadow: 0 2px 5px rgb(var(--shadow-rgb), .1); border-radius: 4px; overflow: hidden; }
 			summary { display: grid; grid-template-columns: 1fr 3rem; padding: .5rem; font-weight: 500; white-space: nowrap; overflow: hidden; text-overflow: ellipsis; background-color: var(--color-gray-lighter); cursor: pointer; transition: background-color .25s ease; }
+			summary::-webkit-details-marker { display:none; }
 			summary svg { width: .85rem; height: 100%; margin-right: .5rem; margin-left: auto; color: var(--color-gray); transform: rotate(0deg); transition: transform .25s ease}
 			summary:hover { background-color: var(--color-gray-light); }
 			::slotted([slot="summary"]) { min-width: 0; max-width: 100%; text-overflow: ellipsis; overflow: hidden; }
