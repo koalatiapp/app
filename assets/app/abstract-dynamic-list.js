17116da98569177e524307f05f9a65d2b1edd5f9
Émile Perron <contact@emileperron.com>
2021-06-30 19:24:35 -0400

    Adds inline documentation for AbstractDynamicList

--- assets/app/abstract-dynamic-list.js
+++ assets/app/abstract-dynamic-list.js
@@ -2,6 +2,25 @@ import { NbList } from "../native-bear";
 import { ApiClient } from "../utils/api";
 import fontawesomeImport from "../utils/fontawesome-import";
 
+/**
+ * Implements the basics for dynamic <nb-list> elemements that
+ * fetch their content from the internal API.
+ *
+ * Has built-in support for live-updates with Mercure with the
+ * custom `suggested-mercure-topic` header returned by the API.
+ *
+ * When extending this class, you must redefine the `fetchListData`
+ * method and call `super.fetchListData()` with the desired endpoint
+ * and request body object.
+ *
+ * Ex.:
+ * ```
+ * fetchListData()
+ * {
+ *   super.fetchListData("api_endpoint_here", { some_param: this.someParam });
+ * }
+ * ```
+ */
 export class AbstractDynamicList extends NbList {
 	render()
 	{
