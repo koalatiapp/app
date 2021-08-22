18b18f90d479ceb7b217678ea7a375bf1a4fa3fd
Émile Perron <contact@emileperron.com>
2021-08-21 21:28:05 -0400

    Adds missing password hashing in registration

--- src/Controller/Public/RegistrationController.php
+++ src/Controller/Public/RegistrationController.php
@@ -8,11 +8,22 @@ use App\Form\UserRegistrationType;
 use App\Security\LoginFormAuthenticator;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\UserAuthenticatorInterface;
 
 class RegistrationController extends AbstractController
 {
+	private UserPasswordHasherInterface $passwordHasher;
+
+	/**
+	 * @required
+	 */
+	public function setPasswordHasher(UserPasswordHasherInterface $passwordHasher): void
+	{
+		$this->passwordHasher = $passwordHasher;
+	}
+
 	/**
 	 * @Route("/sign-up", name="registration")
 	 */
@@ -23,6 +34,10 @@ class RegistrationController extends AbstractController
 		$form->handleRequest($request);
 
 		if ($form->isSubmitted() && $form->isValid()) {
+			$plainPassword = $form->get('password')->getData();
+			$hashedPassword = $this->passwordHasher->hashPassword($user, $plainPassword);
+			$user->setPassword($hashedPassword);
+
 			$em = $this->getDoctrine()->getManager();
 			$em->persist($user);
 			$em->flush();
