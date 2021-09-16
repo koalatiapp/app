0552c56cc4994e001417c9caed37b525bd599bc3
Émile Perron <contact@emileperron.com>
2021-09-16 16:44:01 -0400

    Adds the project status API endpoint

--- src/Controller/Api/Testing/TestingController.php
+++ src/Controller/Api/Testing/TestingController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller\Api\Testing;
 
+use App\ApiClient\Endpoint\StatusEndpoint;
 use App\Controller\Api\AbstractApiController;
 use App\Message\TestingRequest;
 use App\Security\ProjectVoter;
@@ -31,10 +32,34 @@ class TestingController extends AbstractApiController
 			return $this->apiError('You must provide a valid value for `project_id`.');
 		}
 
+		$projectId = $this->idHasher->decode($projectId)[0];
 		$project = $this->getProject($projectId, ProjectVoter::PARTICIPATE);
 
 		$this->dispatchMessage(new TestingRequest($project->getId()));
 
 		return $this->apiSuccess();
+	}
+
+	/**
+	 * Returns data about the project's testing status.
+	 *
+	 * Available query parameters:
+	 * - `project_id` - `int` (required)
+	 *
+	 * @Route("/project-status", methods={"GET"}, name="project_status", options={"expose": true})
+	 */
+	public function projectStatus(Request $request, StatusEndpoint $statusApi): JsonResponse
+	{
+		$projectId = $request->request->get('project_id');
+
+		if (!$projectId) {
+			return $this->apiError('You must provide a valid value for `project_id`.');
+		}
+
+		$projectId = $this->idHasher->decode($projectId)[0];
+		$project = $this->getProject($projectId, ProjectVoter::PARTICIPATE);
+		$status = $statusApi->project($project);
+
+		return $this->apiSuccess($status);
 	}
 }
