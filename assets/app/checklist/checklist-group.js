13bbd698adca19a5834727d35d3d0b88af87a889
Émile Perron <contact@emileperron.com>
2021-09-18 20:22:55 -0400

    Turns checklist groups into collapsible sections

--- assets/app/checklist/checklist-group.js
+++ assets/app/checklist/checklist-group.js
@@ -8,6 +8,16 @@ export class ChecklistGroup extends LitElement {
 		return [
 			stylesReset,
 			css`
+				.group-toggle { text-decoration: none; color: inherit; transition: color .15s ease; }
+				.group-toggle::before { content: '\\f054'; display: inline-block; margin-right: 5px; font-family: var(--fa-style-family); font-size: .7em; font-weight: 400; line-height: 1; transform: rotate(90deg); transform-origin: center; position: relative; top: -1px; transition: transform .25s ease; }
+				.group-toggle:hover,
+				.group-toggle:hover::before { color: var(--color-blue-dark-faded); }
+				.group.loading-items .group-toggle::before { transition: none; }
+
+				.group.closed .group-toggle::before { transform: rotate(0deg); }
+				.group.closed checklist-item-list { display: none; }
+				.group.closed h2 { margin-bottom: 0; }
+
 				.progression { display: inline-block; padding: .35em .75em; margin-left: .35rem; font-size: 0.75rem; font-weight: 500; vertical-align: middle; color: var(--color-blue-dark-faded); background-color: var(--color-blue-10); border-radius: 15px; }
 				.completion-indicator { display: inline-block; margin-left: .35rem; color: var(--color-green); }
 				.completion-indicator:not(.previously-completed) { animation: completion-appearance .25s cubic-bezier(0.25, 0.1, 0.37, 2.5) forwards; }
@@ -30,24 +40,30 @@ export class ChecklistGroup extends LitElement {
 			groupId: {type: String},
 			itemCount: {type: Number},
 			completedItemCount: {type: Number},
+			closed: {type: Boolean},
+			_loaded: {state: true},
 		};
 	}
 
 	constructor()
 	{
 		super();
+		this._loaded = false;
 		this.itemCount = 1;
 		this.completedItemCount = 0;
 		this.previouslyCompleted = false;
+		this.closed = false;
 	}
 
 	render()
 	{
 		return html`
 			${fontAwesomeImport}
-			<div class="group">
+			<div class="group ${this.closed ? "closed" : ""} ${this._loaded ? "" : "loading-items"}">
 				<h2>
-					<slot></slot>
+					<a href="#" class="group-toggle" @click=${(e) => { e.preventDefault(); this.toggle(); }} aria-role="button" aria-expanded=${this.closed ? "false" : "true"} aria-controls="checklist-group-${this.groupId}">
+						<slot></slot>
+					</a>
 					${this.completedItemCount < this.itemCount ? html`
 						<span class="progression">
 							<span class="completed-count">${this.completedItemCount}</span>
@@ -60,7 +76,7 @@ export class ChecklistGroup extends LitElement {
 						</span>
 					`}
 				</h2>
-				<checklist-item-list projectId=${this.projectId} groupId=${this.groupId} @items-initialized=${this._initProgression} @items-updated=${this._updateProgression}></checklist-item-list>
+				<checklist-item-list projectId=${this.projectId} groupId=${this.groupId} @items-initialized=${this._initProgression} @items-updated=${this._updateProgression} aria-hidden=${this.closed ? "true" : "false"} id="checklist-group-${this.groupId}"></checklist-item-list>
 			</div>
 	  	`;
 	}
@@ -74,6 +90,10 @@ export class ChecklistGroup extends LitElement {
 	{
 		this._updateProgression();
 		this.previouslyCompleted = this.itemCount == this.completedItemCount;
+
+		setTimeout(() => {
+			this._loaded = true;
+		}, 10);
 	}
 
 	_updateProgression()
@@ -84,6 +104,13 @@ export class ChecklistGroup extends LitElement {
 		if (this.itemCount != this.completedItemCount) {
 			this.previouslyCompleted = false;
 		}
+
+		this.closed = this.completedItemCount >= this.itemCount;
+	}
+
+	toggle()
+	{
+		this.closed = !this.closed;
 	}
 }
 
