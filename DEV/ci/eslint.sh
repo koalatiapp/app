b2293b04397052c02032b1d943764de3f80de24f
Émile Perron <contact@emileperron.com>
2021-11-16 08:45:25 -0500

    Adds exit to eslint ci script on failure

--- DEV/ci/eslint.sh
+++ DEV/ci/eslint.sh
@@ -1,17 +1,31 @@
+eslint_exit_code=0
+
 # Only run eslint if the public/js/ directory contains files
 if [ -n "$(ls -A public/js/)" ]; then
 	echo "Running ESLint on Javascript files from public/js/..."
 	node_modules/eslint/bin/eslint.js public/js/
+
+	if [ $? -ne 0 ]; then
+		exit 1
+	fi
 fi
 
 # Only run eslint if the assets directory contains files
 if [ -n "$(ls -A assets/)" ]; then
 	echo "Running ESLint on Javascript files from assets/..."
 	node_modules/eslint/bin/eslint.js assets/
+
+	if [ $? -ne 0 ]; then
+		exit 1
+	fi
 fi
 
 # Only run eslint if the templates directory contains files
 if [ -n "$(ls -A templates/)" ]; then
 	echo "Running ESLint on template files..."
 	node_modules/eslint/bin/eslint.js --ext .html,.twig templates/
+
+	if [ $? -ne 0 ]; then
+		exit 1
+	fi
 fi
