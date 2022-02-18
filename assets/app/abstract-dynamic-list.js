02cb646c012daf5365ce08f828a31c9f97e1acf4
Émile Perron <cunrakes@gmail.com>
2022-02-17 21:51:37 -0500

    Fix duplicate loading of data in dynamic lists

--- assets/app/abstract-dynamic-list.js
+++ assets/app/abstract-dynamic-list.js
@@ -22,6 +22,12 @@ import fontawesomeImport from "../utils/fontawesome-import";
  * ```
  */
 export class AbstractDynamicList extends NbList {
+	constructor()
+	{
+		super();
+		this._isWaitingForServerResponse = false;
+	}
+
 	render()
 	{
 		return [
@@ -40,7 +46,7 @@ export class AbstractDynamicList extends NbList {
 		// In some cases, attributes may be set after the initial render,
 		// which causes a list that is "stuck" in the loading state.
 		// By checking on subsequent updates, we can avoid this.
-		if (this.isLoading) {
+		if (this.isLoading && !this._isWaitingForServerResponse) {
 			this.fetchListData();
 		}
 	}
@@ -81,7 +87,10 @@ export class AbstractDynamicList extends NbList {
 	 */
 	fetchListData(endpoint, body = {})
 	{
+		this._isWaitingForServerResponse = true;
+
 		ApiClient.get(endpoint, body).then(response => {
+			this._isWaitingForServerResponse = false;
 			this.items = Array.isArray(response.data) ? response.data : Object.values(response.data);
 
 			this.dispatchEvent(new CustomEvent("items-initialized"));
