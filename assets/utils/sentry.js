b2b78175d0ab82048a5a1955642670556bb6ba88
Émile Perron <contact@emileperron.com>
2022-03-22 18:49:50 -0400

    perf: disable Sentry tracing

--- assets/utils/sentry.js
+++ assets/utils/sentry.js
@@ -1,8 +1,5 @@
 import * as Sentry from "@sentry/browser";
-import { Integrations } from "@sentry/tracing";
 
 Sentry.init({
 	dsn: "https://fa227bdc68d14d56a06247191a71db8b@o1000146.ingest.sentry.io/5959436",
-	integrations: [new Integrations.BrowserTracing()],
-	tracesSampleRate: 0.2,
 });
