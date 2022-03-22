1e23550df3d0ba366d884282883fbbdc462b967e
Émile Perron <contact@emileperron.com>
2022-03-22 18:58:44 -0400

    feat: add environment to Sentry config

--- assets/utils/sentry.js
+++ assets/utils/sentry.js
@@ -2,4 +2,5 @@ import * as Sentry from "@sentry/browser";
 
 Sentry.init({
 	dsn: "https://fa227bdc68d14d56a06247191a71db8b@o1000146.ingest.sentry.io/5959436",
+	environment: window.location.host == "localhost" ? "test" : "prod",
 });
