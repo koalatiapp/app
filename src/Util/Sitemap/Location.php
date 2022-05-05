2224da05d8b9937c8d4d83e2d9825e6d872b84c6
Émile Perron <contact@emileperron.com>
2022-05-05 07:38:38 -0400

    fix: add error 'handling' to page title fetching

--- src/Util/Sitemap/Location.php
+++ src/Util/Sitemap/Location.php
@@ -20,28 +20,48 @@ class Location
 	 */
 	public ?string $title = null;
 
+	/**
+	 * Error code encountered when fetching the page or document.
+	 */
+	public ?int $errorCode = null;
+
 	public function __construct(string $url, ?string $title = null)
 	{
 		$this->url = $url;
 		$this->title = $title;
 	}
 
+	/**
+	 * @SuppressWarnings(PHPMD.ErrorControlOperator)
+	 */
 	public function fetchTitle(): self
 	{
 		if ($this->title) {
 			return $this;
 		}
 
+		$context = stream_context_create([
+			'http' => [
+				'user_agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36 (compatible; KoalatiBot; +https://www.koalati.com/)',
+			]
+		]);
+		libxml_set_streams_context($context);
+
 		$domDocument = new DOMDocument();
 		$domDocument->preserveWhiteSpace = false;
-		libxml_use_internal_errors(true);
-		$domDocument->loadHTMLFile($this->url);
-		libxml_clear_errors();
-		$titleNode = $domDocument->getElementsByTagName('title');
-		$title = trim($titleNode->item(0)?->textContent);
 
-		if ($title) {
-			$this->title = $title;
+		// DOMDocument::loadHTMLFile throws uncatchable errors when it hits 403/404/500/etc.
+		libxml_use_internal_errors(true);
+		$documentIsLoaded = @$domDocument->loadHTMLFile($this->url);
+		libxml_clear_errors();
+
+		if ($documentIsLoaded) {
+			$titleNode = $domDocument->getElementsByTagName('title');
+			$title = trim($titleNode->item(0)?->textContent);
+
+			if ($title) {
+				$this->title = $title;
+			}
 		}
 
 		return $this;
