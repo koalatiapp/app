bab608c5e15f8119498abcf6ca59f93c594625d2
Émile Perron <contact@emileperron.com>
2022-05-18 21:15:57 -0400

    fix: improve favicon fetching to add a default icon that says 'no icon' (#76)
    * fix(testing): prevent rtrim() on null value, which threw warnings
    
    * fix: add default favicon indicating 'no icon'

--- src/Util/Favicon/FaviconFetcher.php
+++ src/Util/Favicon/FaviconFetcher.php
@@ -9,8 +9,6 @@ use Throwable;
 
 class FaviconFetcher implements FaviconFetcherInterface
 {
-	private LoggerInterface $logger;
-
 	/**
 	 * @param array<int,FaviconDriverInterface> $drivers
 	 */
@@ -19,24 +17,16 @@ class FaviconFetcher implements FaviconFetcherInterface
 	) {
 	}
 
-	/**
-	 * @required
-	 */
-	public function setLogger(LoggerInterface $logger): void
-	{
-		$this->logger = $logger;
-	}
-
 	public function fetch(string $url): string
 	{
 		foreach ($this->drivers as $driver) {
 			try {
 				return $driver->fetch($url);
 			} catch (Throwable $exception) {
-				$this->logger->error($exception, debug_backtrace());
+				// Oh well, no icon from this driver.
 			}
 		}
 
-		throw new Exception(sprintf("Could not fetch favicon for '$url'. %d favicon drivers were attempted, and they all failed.", count($this->drivers)));
+		return base64_decode("iVBORw0KGgoAAAANSUhEUgAAADQAAAA0CAMAAADypuvZAAAAMFBMVEUAAAD/KTr/KTr/KTr/KTr/KTr/KTr/KTr/KTr/KTr/KTr/KTr/KTr/KTr/KTr/KTp9OfapAAAAD3RSTlMAESIzRFVmd4iZ7t3Mu6q3AupnAAABcklEQVR4Ae3TBw7cQAiFYRiX6fvf/7aJjdGSNlFU0p8aWHzuyD+S/1HVUFjSlpZmB8o9COx3URvQiq4RW0BbwzJ0jYYjN83VApEdaYN2imgFXmvE9qATOOTKpdICdRgPekGXK3d/LtDeIBsaMMUClAVK5wW+gvIKyYTut9fkzgYcS5Qa3OjAzz8BXSI5HyQdKEm2F1BljWQ+KHU8U76HUjPkys0SSX6QaLnYOOUHo+mX7+v7VlRCdPPehkIx/b/PHWgvf4ZzAMxdnqF2T1cYAb14cvo6WA5DNhVRNHC4ac1PMvHjERVgJkkF6CLZWtk7sD2o6WeoWfNRzzmT9Iv6/lVD91xEW9zNuKq2v74vh0S02114QpsBG6rQdY3YrSw40g41orS8vXDmHtD7yesYI92t+qZXH6pARPnqdt2qHT2t1bMByYe0RxT/AHuaQmx9aCeioNruby20cSgiOWaDXlTkaS9SN4lIu6EY/WJp/rX8zwfIHCTWafSg+gAAAABJRU5ErkJggg==");
 	}
 }
