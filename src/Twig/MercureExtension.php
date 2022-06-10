98b87910dc83885a60b648a384f83a23adf58dcb
Émile Perron <contact@emileperron.com>
2022-06-10 17:28:58 -0400

    fix: increase mercure expiration to 1 day after pageload

---
+++ src/Twig/MercureExtension.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Twig;
+
+use App\Entity\User;
+use App\Mercure\UserTopicBuilder;
+use DateTimeImmutable;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+
+class MercureExtension extends AbstractExtension
+{
+	public function __construct(
+		private UserTopicBuilder $userTopicBuilder,
+	) {
+	}
+
+	public function getFunctions(): array
+	{
+		return [
+			new TwigFunction('mercure_topic', [$this, 'getMercureTopic']),
+			new TwigFunction('mercure_config', [$this, 'getMercureConfig']),
+		];
+	}
+
+	public function getMercureTopic(User $user): string
+	{
+		return $this->userTopicBuilder->getTopic($user);
+	}
+
+	/**
+	 * @return array<string,mixed>
+	 */
+	public function getMercureConfig(User $user): array
+	{
+		return [
+			"subscribe" => $this->getMercureTopic($user),
+			"additionalClaims" => [
+				"exp" => new DateTimeImmutable("+1 day"),
+			],
+		];
+	}
+}
