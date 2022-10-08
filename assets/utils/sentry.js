e4d23097589c2a8550b61bc81bb7f1a888674072
Émile Perron <contact@emileperron.com>
2022-10-08 17:04:06 -0400

    fix: check for Sentry config before initializing it

--- assets/utils/sentry.js
+++ assets/utils/sentry.js
@@ -1,6 +1,8 @@
 import * as Sentry from "@sentry/browser";
 
-Sentry.init({
-	dsn: env.SENTRY_DSN,
-	environment: env.APP_ENV,
-});
+if (typeof env != "undefined" && env?.SENTRY_DSN) {
+	Sentry.init({
+		dsn: env.SENTRY_DSN,
+		environment: env.APP_ENV,
+	});
+}
