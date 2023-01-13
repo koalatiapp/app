44fb9cda522ee43221c12c7487815c6614f1e2d3
Émile Perron <contact@emileperron.com>
2023-01-13 16:45:25 -0500

    fix: throw 401 error when session user differs from jwt user

--- src/Api/Security/AuthenticationJwtListener.php
+++ src/Api/Security/AuthenticationJwtListener.php
@@ -5,12 +5,16 @@ namespace App\Api\Security;
 use App\Entity\User;
 use App\Subscription\PlanManager;
 use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTCreatedEvent;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\JWTDecodedEvent;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class AuthenticationJwtListener
 {
 	public function __construct(
 		private PlanManager $planManager,
+		private RequestStack $requestStack,
 	) {
 	}
 
@@ -37,6 +41,28 @@ class AuthenticationJwtListener
 			}
 
 			throw new AccessDeniedException("You may not use the Koalati API because your subscription plan does not permit it.");
+		}
+	}
+
+	public function onJwtDecoded(JWTDecodedEvent $event): void
+	{
+		$request = $this->requestStack->getCurrentRequest();
+
+		/*
+		 * If the request contains a session cookie, check to make sure the
+		 * session's user matches the JWT's user. If it does not, throw a 401
+		 * to let the client know they have to update their authentication.
+		 *
+		 * This should never occur when using the API as usual, but it can
+		 * happen if a user of the app logs out and then logs in with a
+		 * different user before their client-stored JWT expires.
+		 */
+		if ($request->cookies->has(session_name())) {
+			$session = $this->requestStack->getSession();
+
+			if ($event->getPayload()['username'] != $session->get('_security.last_username')) {
+				throw new UnauthorizedHttpException("bearer", "User session does not match the provided JWT. Please re-authenticate.");
+			}
 		}
 	}
 }
