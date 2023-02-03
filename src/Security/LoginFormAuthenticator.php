9588f1d741daa85e03e50392dc2d061c47a1d38e
Émile Perron <contact@emileperron.com>
2023-02-03 13:23:14 -0500

    feat: add rate limiting to app login

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -8,9 +8,11 @@ use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\RateLimiter\RateLimiterFactory;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\TooManyLoginAttemptsAuthenticationException;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
@@ -30,12 +32,15 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 		private readonly EntityManagerInterface $entityManager,
 		private readonly EmailVerifier $emailVerifier,
 		private readonly TokenStorageInterface $tokenStorage,
+		private readonly RateLimiterFactory $appAuthenticationLimiter,
 	) {
 	}
 
 	public function authenticate(Request $request): Passport
 	{
 		$email = $request->request->get('email', '');
+
+		$this->checkRateLimiting($request, $email);
 
 		$request->getSession()->set(Security::LAST_USERNAME, $email);
 
@@ -47,6 +52,16 @@ class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 				new RememberMeBadge(),
 			]
 		);
+	}
+
+	private function checkRateLimiting(Request $request, string $email): void
+	{
+		$rateLimiter = $this->appAuthenticationLimiter->create(strtolower(trim($email))."-".$request->getClientIp());
+		$limit = $rateLimiter->consume(1);
+
+		if ($limit->isAccepted() === false) {
+			throw new TooManyLoginAttemptsAuthenticationException(threshold: (int) ceil(($limit->getRetryAfter()->getTimestamp() - time()) / 60));
+		}
 	}
 
 	/**
