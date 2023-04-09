a50960415a30a03c5a5e683783a1603fdd2249ac
Émile Perron <contact@emileperron.com>
2023-04-09 09:21:38 -0400

    fix: ensure comment threads can be deleted withotu log errors

--- src/Activity/Logger/CommentLogger.php
+++ src/Activity/Logger/CommentLogger.php
@@ -34,11 +34,17 @@ class CommentLogger extends AbstractEntityActivityLogger
 
 	public function postRemove(object &$comment): void
 	{
+		// If we don't clear the child relations, Doctrine detects non-persisted relations
+		// ... but these are cleared by the database's ON CASCADE DELETE
+		foreach ($comment->getReplies() as $reply) {
+			$reply->setThread(null);
+		}
+
 		$this->log(
 			type: "comment_delete",
 			organization: $comment->getProject()->getOwnerOrganization(),
 			project: $comment->getProject(),
-			target: null,
+			target: $comment->getChecklistItem(),
 		);
 	}
 }
